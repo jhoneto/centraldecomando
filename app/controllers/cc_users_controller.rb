4860dc5127a05b344ee01693e63f10e2d211bb25
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2012-03-06 23:05:04 -0300

    Retirar controle de acesso ao cadastro de usuário

--- app/controllers/cc_users_controller.rb
+++ app/controllers/cc_users_controller.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 class CcUsersController < ApplicationController
-  load_and_authorize_resource
+  #load_and_authorize_resource
   layout "bootstrap_full_content"
   before_filter :authorize
   
