d9be9f9f997ab929ca68c92be1b8dea7ac88cb64
Hamilton Oliveira <hamilton@npxtech.com.br>
2022-03-04 09:41:22 -0300

    teste

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery
   before_filter :authorize
-  
+
   #def authenticate(params)
   #  User.authenticate(params[:session][:username],
   #                    params[:session][:password])
   #end
+
+  # adicoonando ...
 end
